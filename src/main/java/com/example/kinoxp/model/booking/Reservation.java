60e633c01d6fa208dae75d93ad71a4503db1fd5c
hjalteklarsen <73437236+hjalteklarsen@users.noreply.github.com>
2025-09-30 10:57:18 +0200

    test

--- src/main/java/com/example/kinoxp/model/booking/Reservation.java
+++ src/main/java/com/example/kinoxp/model/booking/Reservation.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Reservation {
-    
+    //test
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer reservationId;
