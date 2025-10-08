da7d54982faaf709402ab66b6e0451a6eae8f148
hjalteklarsen <73437236+hjalteklarsen@users.noreply.github.com>
2025-10-08 10:42:37 +0200

    CORS

--- src/main/java/com/example/kinoxp/CORS/CorsConfig.java
+++ src/main/java/com/example/kinoxp/CORS/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins("http://localhost:63342") //Her skal vi have vores Front-ends url
                 .allowedMethods("GET","POST","PUT","DELETE");
     }
