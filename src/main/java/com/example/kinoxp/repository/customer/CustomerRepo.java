6305d71394042a6cff31bdf979900f79a44f9fd0
hjalteklarsen <73437236+hjalteklarsen@users.noreply.github.com>
2025-10-20 12:19:57 +0200

    Add Optional import for findByPhone method

--- src/main/java/com/example/kinoxp/repository/customer/CustomerRepo.java
+++ src/main/java/com/example/kinoxp/repository/customer/CustomerRepo.java
@@ -3,6 +3,8 @@ package com.example.kinoxp.repository.customer;
 import com.example.kinoxp.model.customer.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerRepo extends JpaRepository<Customer,Integer> {
   Optional<Customer> findByPhone(String phone);
 }
